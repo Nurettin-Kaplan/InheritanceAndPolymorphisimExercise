15720dfbf4cd708cd92dd3fe0a14ad4ece838c23
Nurettin-Kaplan <nurettinkaplaan@gmail.com>
2024-08-21 14:29:16 +0300

    The class named  was coded.

--- InheritanceAndPolymorphisimExercise.cpp
+++ InheritanceAndPolymorphisimExercise.cpp
@@ -2,6 +2,28 @@
 
 using namespace std;
 
+class Person {
+protected:
+    string name;
+    int age;
+public:
+    void setName(string value) {
+        this->name = value;
+    }
+    string getName() {
+        return name;
+    }
+    void setAge(int value) {
+        this->age = value;
+    }
+    int getAge() {
+        return age;
+    }
+    Person() {
+        cout << "An instance has been derived from Person." << endl;
+    }
+};
+
 int main(){
 
 
